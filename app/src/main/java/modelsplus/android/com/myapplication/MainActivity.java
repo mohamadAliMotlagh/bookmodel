d7dd2089dcad22bda408c98b43b33ebc4d2657cb
hosein.motlagh <ali.highboy@yahoo.com>
2017-06-28 04:27:05 +0430

    a2 Added
    a2 added to main

--- app/src/main/java/modelsplus/android/com/myapplication/MainActivity.java
+++ app/src/main/java/modelsplus/android/com/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("sssssssssssss","a1");
+        Log.d("sssssssssssss","a2");
     }
 }
