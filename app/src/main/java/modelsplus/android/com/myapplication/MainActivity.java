1e4533ce67c5c60dcf6e73d771a20b36681887fc
hosein.motlagh <ali.highboy@yahoo.com>
2017-06-28 05:06:22 +0430

    a5 added

--- app/src/main/java/modelsplus/android/com/myapplication/MainActivity.java
+++ app/src/main/java/modelsplus/android/com/myapplication/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         Log.d("sssssssssssss","a33");
         Log.d("sssssssssssss","a44444444444");
         Log.d("sssssssssssss","a4444444444");
+        Log.d("sssssssssssss","a5");
     }
 }
